183fd9b9a1722d5d2eac8203a9ba1b2a15a5b2d6
Alexander Kram <alexander.kram@gmx.de>
2024-01-26 04:30:06 +0100

    lecture plan but new

--- main.py
+++ main.py
@@ -10,12 +10,14 @@ import pytz
 import requests
 import pyhlia_lecture
 
-
 # loading token
 with open('token.txt') as file:
     token = file.readlines()
 
+# bot declaration
 bot = commands.Bot(command_prefix="!",intents = discord.Intents.all())
+counter = 0
+lecturecounter = 0
 
 # ready-message
 @bot.event
@@ -27,177 +29,15 @@ async def on_ready():
     except Exception as e:
         print(e)
 
+@bot.tree.command(name="lecture")
+async def lecture(interaction: discord.ui.Button):
+    print(datetime.today())
+    printing_lecture = await lecture_data(date.today())
+    await interaction.response.send_message(embed=printing_lecture)
 
-data = None
-counter = 0
-day = datetime.today()
 
-
-class MyView2(discord.ui.View):
-               @discord.ui.select( # the decorator that lets you specify the properties of the select menu 
-                    placeholder = "Choose a month!", # the placeholder text that will be displayed if nothing is selected
-                    min_values = 1, # the minimum number of values that must be selected by the users
-                    max_values = 1, # the maximum number of values that can be selected by the users
-                    options = [ # the list of options from which users can choose, a required field
-                    discord.SelectOption(
-                        label="January",
-                        description="first month"
-                    ),
-                    discord.SelectOption(
-                        label="February",
-                        description="second month"
-                    ),
-                    discord.SelectOption(
-                        label="March",
-                        description="third month"
-                    ),
-                    discord.SelectOption(
-                        label="April",
-                        description="fourth month"
-                    ),
-                    discord.SelectOption(
-                        label="May",
-                        description="fifth month"
-                    ),
-                    discord.SelectOption(
-                        label="June",
-                        description="sixth month"
-                    ),
-                    discord.SelectOption(
-                        label="July",
-                        description="seventh month"
-                    ),
-                    discord.SelectOption(
-                        label="August",
-                        description="eighth month"
-                    ),
-                    discord.SelectOption(
-                        label="September",
-                        description="nineth month"
-                    ),
-                    discord.SelectOption(
-                        label="October",
-                        description="tenth month"
-                    ),
-                    discord.SelectOption(
-                        label="November",
-                        description="eleventh"
-                    ),
-                    discord.SelectOption(
-                        label="Dezember",
-                        description="twelveth month"
-                    )
-                        ]
-                    )
-               # this function is triggered when you hit the month-menu, the function defines the buttons new
-               async def select_callback(self,interaction,select): # the function called when the user is done selecting options
-                  if select.values[0] == "January":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,1))
-                  elif select.values[0] == "February":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,2))
-                  elif select.values[0] == "March":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,3))
-                  elif select.values[0] == "April":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,4))
-                  elif select.values[0] == "May":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,5))
-                  elif select.values[0] == "June":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,6))
-                  elif select.values[0] == "July":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,7))
-                  elif select.values[0] == "August":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,8))
-                  elif select.values[0] == "September":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,9))
-                  elif select.values[0] == "October":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,10))
-                  elif select.values[0] == "November":
-                        await interaction.response.defer()
-                        await buttons_message.edit(view=await create_buttons(interaction,11))
-                  else:
-                        await buttons_message.edit(view=await create_buttons(interaction,12))
-                        await interaction.response.defer()
-
-class MyView(discord.ui.View):
-               @discord.ui.select( # the decorator that lets you specify the properties of the select menu 
-                    placeholder = "Choose a week!", # the placeholder text that will be displayed if nothing is selected
-                    min_values = 1, # the minimum number of values that must be selected by the users
-                    max_values = 1, # the maximum number of values that can be selected by the users
-                    options = [ # the list of options from which users can choose, a required field
-                    discord.SelectOption(
-                        label="Week 1",
-                        description= f"{day} - {day +timedelta(days=4)}"
-                    ),
-                    discord.SelectOption(
-                        label="Week 2",
-                        description="08.01-12.01"
-                    ),
-                    discord.SelectOption(
-                        label="Week 3",
-                        description="15.01-19.01"
-                    ),
-                    discord.SelectOption(
-                        label="Week 4",
-                        description="22.01-26.01"
-                    ),
-                    discord.SelectOption(
-                        label="Week 5",
-                        description="29.01-03.02"
-                    )   
-                        ]
-                    )
-               async def select_callback(self,interaction,select): # the function called when the user is done selecting options
-                  if select.values[0] == "Week 1":
-                        await interaction.response.send_message("Cool")
-                  elif select.values[0] == "Week 2":
-                        await interaction.response.send_message("Cool")
-                  elif select.values[0] == "Week 3":
-                        await interaction.response.send_message("Cool")
-                  elif select.values[0] == "Week 4":
-                        await interaction.response.send_message("Cool")
-                  elif select.values[0] == "Week 5":
-                        await interaction.response.send_message("Cool")
-                  
-async def create_buttons(interaction: discord.Interaction,month):
-      view4 = View()
-      year = datetime.today().strftime("%Y")
-      global day
-      day = datetime(int(year), month, 1, 12, 0, 0, 0)
-      for j in range(0,5):
-                     if j % 2 != 0:                         
-                            temp2 = j
-                            j = (Button(custom_id = f"{interaction.id}~{day+timedelta(days=temp2)}",style=discord.ButtonStyle.green, label=str((day+timedelta(days=j)).strftime("%d.%m")),row=1))
-                            view4.add_item(j)                             
-                            j.callback = lambda j: callback2(j)       
-                     else:
-                            temp2 = j
-                            j = (Button(custom_id = f"{interaction.id}~{day+timedelta(days=temp2)}",style=discord.ButtonStyle.blurple, label=str((day+timedelta(days=j)).strftime("%d.%m")),row=1))
-                            view4.add_item(j)
-                            j.callback = lambda j: callback2(j)
-      return view4
-
-async def callback2(interaction: discord.Interaction):
-                      #date_entry=date.today()+ timedelta(days=1)####################
-                   # try:
-                      print(interaction.data)
-                      date = interaction.data['custom_id'].split('~')[1]
-                      await lecture_data(datetime.strptime(date,"%Y-%m-%d %H:%M:%S").date())
-                      await interaction.response.defer()
-      
 # method called by buttonsclass (copied)
 async def lecture_data(date_entry):
-    print("erreicht")
-    global data
     cal_url = "https://stuv.app/MOS-TINF23A/ical"
     target_date = date_entry #style 2023, 12, 20
     print(date_entry)
@@ -216,6 +56,9 @@ async def lecture_data(date_entry):
                         #print(start_time)
                         #print(target_date)
                         if start_time.date() == target_date:
+                            global lecturecounter
+                            lecturecounter += 1
+
                             summary = event.get('summary')
                             end_time = event.get('dtend').dt
                             room = event.get('location')
@@ -235,96 +78,48 @@ async def lecture_data(date_entry):
                                 'start_time': start_time.strftime("%H:%M" + ' Uhr'),
                                 'end_time': end_time.strftime("%H:%M" + ' Uhr'),
                                 'room': room
-                            })                           
-    # printing out all lectures for the fitting date through discord-embeds
-    for event in (target_date_events):
-                            channel = bot.get_channel(1184076609779671111)
+                            })
+
+                lecture_lists = []       
+                # printing out all lectures for the fitting date through discord-embeds
+                for event in (target_date_events):
                             embed = discord.Embed(
-                            title = "**"+str(event['summary'])+"**",
-                            #description=date,
-                            color=discord.Color.blurple()  # You can set the color of the embed
+                            title = f"**Vorlesung vom *{date_entry.strftime("%d.%m.%Y")}***",
+                            
+                            description="\n\n",
+                            color=0xD9A4FC
+  # You can set the color of the embed
+                            
                             )
                             # Add fields to the embed
-                            embed.add_field(name='__Beginn:__', value=event['start_time'], inline=False)
-                            embed.add_field(name='__Ende:__', value=event['end_time'], inline=True)
-                            embed.add_field(name='__Vorlesungsort:__', value=event['room'], inline=False)
-                            embed.set_image(url="https://cdn.discordapp.com/attachments/909054108235862066/1197594048781893694/541px-DHBW-Logo.png?ex=65bbd55f&is=65a9605f&hm=8d57652450766fa4a11d1dc6ff195858d72c83b76dda26e898e8a59d1b8606a1&")
+                            lecture_info = [
+                                event['summary'],
+                                event['start_time'],
+                                event['end_time'],
+                                event['room']
+                            ]
+                            lecture_lists.append(lecture_info)
                             
-                            print(f"Summary: {event['summary']}")
-                            print(f"Start Time: {event['start_time']}")
-                            print(f"End Time: {event['end_time']}")
-                            print(f"Room: {event['room']}")
-                            print("-----")
-                            await channel.send(embed=embed)
-                                                       
-# copied
-def jan1():
-  year = datetime.today().strftime("%Y")
-  jan1 = datetime(int(year), 1, 1, 0, 0, 0, 0).weekday()
-  return jan1
-# copied
-
-@bot.tree.command(name="buttontest")
-async def lecture(interaction: discord.ui.Button):
-    channel = bot.get_channel(1184076609779671111)
-    global data
-    view = await buttons(interaction)
-    
-    await interaction.response.send_message(view=MyView2())
-    global buttons_message
-    buttons_message = await channel.send(view=view)
-    await channel.send(view=MyView())
-    
-   
-    
-    
-
-# copied
-async def buttons(interaction: discord.Interaction):
-       global data
-       data = interaction
-       # getting the first monday in the year
-       for i in range(0,6):
-              if jan1() == 0:
-               year = datetime.today().strftime("%Y")
-               firstmonday = datetime(int(year), 1, 1, 12, 0, 0, 0) + timedelta(days=i)
-              else:
-               firstmonday = datetime(int(year), 1, 1, 12, 0, 0, 0) + timedelta(days=7-i)
-              view = View()
-              # callback for buttons
-              async def callback2(interaction: discord.Interaction):
-                      #date_entry=date.today()+ timedelta(days=1)####################
-                   # try:
-                      print(interaction.data)
-                      date = interaction.data['custom_id'].split('~')[1]
-                      await lecture_data(datetime.strptime(date,"%Y-%m-%d %H:%M:%S").date())
-                      await interaction.response.defer()
-              
-              # creating buttons
-              for j in range(0,5):
-                     if j % 2 != 0:
-                            temp2 = j
-                            j = (Button(custom_id = f"{interaction.id}~{firstmonday+timedelta(days=temp2)}",style=discord.ButtonStyle.green, label=str((firstmonday+timedelta(days=j)).strftime("%d.%m")),row=1))
-                            view.add_item(j)                             
-                            j.callback = lambda j: callback2(j)                      
-                     else:
-                            temp2 = j
-                            j = (Button(custom_id = f"{interaction.id}~{firstmonday+timedelta(days=temp2)}",style=discord.ButtonStyle.blurple, label=str((firstmonday+timedelta(days=j)).strftime("%d.%m")),row=1))
-                            view.add_item(j)
-                            j.callback = lambda j: callback2(j)
+                            print("reached")
+                            time = str(lecture_lists[0][1]) + " - " + str(lecture_lists[0][2])
+                            print(time)
+                           
                             
-              
-              return view
-        
-                                 
-              
-        
-async def test(ctx):
-    global data3
-    data3 = ctx
-    return data3
-
-
+                print(lecture_lists)#═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═ ═
+                for i in range(lecturecounter):
+                    
+                      embed.add_field(name="══════════════════════════════",value='',inline=True)
+                      embed.add_field(name="__"+lecture_lists[i][0]+"__:",value="- Beginn: "+"*"+lecture_lists[i][1]+"*"+ "   ┃   Ende: *"+lecture_lists[i][2]+"*"+ "\n"+"- Raum: *"+lecture_lists[i][3]+"*", inline=False)
+                      embed.set_footer(text=f"Made by" + " @ragednicmaster"+" and Alex")
+                    
+                      embed.set_footer(text="‎ ",icon_url="https://cdn.discordapp.com/attachments/909054108235862066/1178115964152328232/guycoding.png?ex=65beccfe&is=65ac57fe&hm=e434044069bea56e66c39fd651e713d7f189e84f069ad0f9ea89c98a012eaaac&")
+                embed.set_image(url="https://cdn.discordapp.com/attachments/909054108235862066/1200276858088988682/541px-DHBW-Logo-2-2.webp?ex=65c597ef&is=65b322ef&hm=d04afcf8052adc2620286cc74f3fd58512542d6ce186ab5dd60d98f088335a9a&")
+                
+                            
+                return embed
+                            
+                     #┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄       
+                
 # running bot with token
 bot.run(token[0])
 
