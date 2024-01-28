6494d59e130e54da4029f1830eae2bf7271674d7
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2024-01-28 23:13:58 +0100

    button with class - not working; startMsg send + Button

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -12,7 +12,11 @@ bot = commands.Bot(command_prefix="!",intents = discord.Intents.all())
 # ready-message
 @bot.event
 async def on_ready():
-    print("I am ready for work! ~Pyhlia")
+    print(f'{bot.user} is now purring!')
+    status = discord.CustomActivity("Ich werde gerade programmiert ._.")
+    await bot.change_presence(status=discord.Status.online, activity=status)
+    print(f"Status set to: {status}!")
+
     try:
         synced = await bot.tree.sync()
         print(f"Currently listening to {len(synced)} command(s).")
@@ -27,20 +31,37 @@ async def on_ready():
 mainColour = 0xa2c188
 embedGamesColour = 0xF5E6FE
 
+async def startTTT(event: discord.Interaction):
+    embedTTT=discord.Embed(title="TTT-Game search", 
+                           description=f"{event.user.mention} want's to play a TicTacToe game. \nWho want's to play against {event.user.mention}", 
+                           color=embedGamesColour)
+    await event.response.send_message(embed=embedTTT, ephemeral=False, view=clickMe())
+
+
+
 class clickMe(discord.ui.View): # Create a class called clickMe that subclasses discord.ui.View
     @discord.ui.button(label="I want to play!", style=discord.ButtonStyle.grey, emoji="✅")
-    async def button_callback(self, interaction:discord.Interaction, button):
-        await interaction.message.edit()
+    async def button_callback(self, interaction:discord.Interaction, button, messageStart:discord.Interaction, startPlayer:discord.Interaction.user):
+        await messageStart.message.edit(title="TTT-Game started", 
+                           description=f"{startPlayer.mention} is going to play against {interaction.user.mention}\nWho want's to play against {interaction.user.mention}", 
+                           color=embedGamesColour)
         await interaction.response.send_message(f"{interaction.user.mention} clicked the button!") # Send a message when the button is clicked
+
 
 
 @bot.tree.command (name="tictactoe", description="TicTacToe")
 async def ttt(interaction: discord.Interaction):
-    embedTTT=discord.Embed(title="TTT-Game started", 
-                           description=f"{interaction.user.mention} want's to play a TicTacToe game. \nWho want's to play against {interaction.user.mention}", 
-                           color=embedGamesColour)
-    embedTTT.add_field(name="Field 1", value="bla bla bla", inline=True)
-    await interaction.response.send_message(embed=embedTTT, ephemeral=False, view=clickMe())
+    startPlayer = interaction.user
+    startMsg = await startTTT(interaction)
+    startMsgID = startMsg.id
+
+    #msg edit
+    #game start
+        #input 1
+            #test
+        #input 2
+            #test
+    #winner msg edit
 
 
 # async def checkWin(activPlayer, ):
