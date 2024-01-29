be7ac6c0034e9d89de3894ced133dd6eae48296b
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2024-01-29 23:34:50 +0100

    msgEdit with right Users - no User verfication and no return to mainFunction

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,9 +5,9 @@ from discord.ext import commands
 # loading token
 with open('token.txt') as file:
     token = file.readlines()
-    
+
 # bot declaration
-bot = commands.Bot(command_prefix="!",intents = discord.Intents.all())
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 # ready-message
 @bot.event
@@ -22,61 +22,52 @@ async def on_ready():
         print(f"Currently listening to {len(synced)} command(s).")
     except Exception as e:
         print(e)
-    #user = await bot.fetch_user("721404393664020560")
-    #await user.send("Hello there, I am online now!")
-
-#for j in range(1,10):
-#    print(j)
 
 mainColour = 0xa2c188
-embedGamesColour = 0xF5E6FE
-
-async def startTTT(event: discord.Interaction):
-    embedTTT=discord.Embed(title="TTT-Game search", 
-                           description=f"{event.user.mention} want's to play a TicTacToe game. \nWho want's to play against {event.user.mention}", 
-                           color=embedGamesColour)
-    await event.response.send_message(embed=embedTTT, ephemeral=False, view=clickMe())
+embedGamesColour = 0xd9a4fc
 
 
+class start_button(discord.ui.View):
+    def __init__(self, player1_id, **kwargs):
+        super().__init__(**kwargs)
+        self.player1_id = player1_id
 
-class clickMe(discord.ui.View): # Create a class called clickMe that subclasses discord.ui.View
-    @discord.ui.button(label="I want to play!", style=discord.ButtonStyle.grey, emoji="✅")
-    async def button_callback(self, interaction:discord.Interaction, button, messageStart:discord.Interaction, startPlayer:discord.Interaction.user):
-        await messageStart.message.edit(title="TTT-Game started", 
-                           description=f"{startPlayer.mention} is going to play against {interaction.user.mention}\nWho want's to play against {interaction.user.mention}", 
-                           color=embedGamesColour)
-        await interaction.response.send_message(f"{interaction.user.mention} clicked the button!") # Send a message when the button is clicked
+    @discord.ui.button(custom_id="challenge_start_button", label="I want to play!", style=discord.ButtonStyle.grey, row=1, emoji="<a:haken:1024262765721948251>")
+    async def challenge_start_callback(self, interaction:discord.Interaction, button):
+        # Get the user who clicked the button (Player2)
+        player2 = interaction.user
+
+        # Get the information about the command initiator (Player1)
+        player1 = await bot.fetch_user(self.player1_id)
+
+        # Update the message with the new embed mentioning both players
+        embed_challenge_accept = discord.Embed(
+            title="TTT-Game started",
+            description=f"<@{player1.id}> is playing against {player2.mention}",
+            color=embedGamesColour
+        )
+
+        # Update the message with the new embed
+        await interaction.message.edit(embed=embed_challenge_accept, view=None)
 
 
+async def startTTT(interaction: discord.Interaction):
+    player1 = interaction.user
+    embedTTT = discord.Embed(
+        title="TTT-Game search",
+        description=f"{player1.mention} wants to play a TicTacToe game.\nWho wants to play against {player1.mention}",
+        color=embedGamesColour
+    )
+    await interaction.response.send_message(embed=embedTTT, ephemeral=False, view=start_button(player1.id))
+    return player1
 
-@bot.tree.command (name="tictactoe", description="TicTacToe")
+
+@bot.tree.command(name="tictactoe", description="TicTacToe")
 async def ttt(interaction: discord.Interaction):
+    # Get the user who triggered the slash command
     startPlayer = interaction.user
     startMsg = await startTTT(interaction)
-    startMsgID = startMsg.id
 
-    #msg edit
-    #game start
-        #input 1
-            #test
-        #input 2
-            #test
-    #winner msg edit
-
-
-# async def checkWin(activPlayer, ):
-
-#     return
-
-# async def checkPlayer():
-#     #if input.user=activPlayer
-
-# async def rightPlayer():
-# async def wrongPlayer():
-
-# async def renderField():
-#     for j in range(1,10):
-#         print(j)
 
 # running bot with token
 bot.run(token[0])
