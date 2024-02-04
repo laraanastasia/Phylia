9d567a92695bfe61e819c1a14883eb4f663a04db
NicoAtSchwarz <nico.oblaender@mail.schwarz>
2024-02-04 23:00:57 +0100

    completly working game - not well pentested

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -31,12 +31,17 @@ class start_button(discord.ui.View):
         super().__init__(**kwargs)
         self.player1_id = player1_id
         self.player2 = None
+        self.message = None
+        self.current_player = None
 
     @discord.ui.button(custom_id="challenge_start_button", label="I want to play!", style=discord.ButtonStyle.grey, row=1, emoji="<a:haken:1024262765721948251>")
     async def challenge_start_callback(self, interaction:discord.Interaction, button):
         player1 = await bot.fetch_user(self.player1_id)
         self.player2 = interaction.user  # Set the player2 property here
+        self.current_player = player1  # Set the current player to player1
         player2 = interaction.user
+
+        self.message = interaction.message
 
         # Update the message with the new embed mentioning both players
         embed_challenge_accept = discord.Embed(
@@ -47,13 +52,9 @@ class start_button(discord.ui.View):
 
         # Update the message with the new embed
         await interaction.message.edit(embed=embed_challenge_accept, view=None)
-        # Neue Nachricht + 9 Buttons
+        # New message + 9 buttons
         gameField = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        await self.send_game_start_message(interaction.channel, player1, player2, gameField)
-
-    async def send_game_start_message(self, channel, player1, player2, gameField):
-        await channel.send("Game started", view=TicTacToe(player1, player2, gameField))
-
+        await interaction.channel.send(f"It's {player1.mention}'s turn.", view=TicTacToe(player1, player2, gameField))
 
 class TicTacToe(discord.ui.View):
     def __init__(self, player1, player2, gameField, **kwargs):
@@ -61,11 +62,11 @@ class TicTacToe(discord.ui.View):
         self.player1 = player1
         self.player2 = player2
         self.gameField = gameField
-
+        self.current_player = player1  # Set the current player to player1 at start to player1
+        
         # Create 9 buttons
         for i in range(1, 10):
             button = discord.ui.Button(custom_id=str(i), label=str(i), style=discord.ButtonStyle.secondary, row=(i - 1) // 3)
-            print(f"Button {i} created: {button}")
             self.add_item(button)
             button.callback = lambda interaction, button=button: self.button_callback(interaction, button)
 
@@ -74,36 +75,37 @@ class TicTacToe(discord.ui.View):
         print(f"Button_callback {button.custom_id} was clicked by {interaction.user}")
         player1 = self.player1
         player2 = self.player2
-        print(f"Player 1 - button_callback: {player1}")
-        print(f"Player 2 - button_callback: {player2}")
         gameField = self.gameField
+        current_player = self.current_player
 
-        if interaction.user == player1 or interaction.user == player2:
+        if interaction.user == current_player:  # Check if the current player is making the move
             newView = await self.update_game_field(button, 1 if interaction.user == player1 else 2)
-            await interaction.response.edit_message(view=newView)
             winner = await self.check_winner()
-            if winner:
-                await interaction.channel.send(f"{winner.mention} won the game!")
+            if winner == player1 or winner == player2:
+                await interaction.response.edit_message(content=f"{winner.mention} won the game! \nCongratulations :tada: !", view=None)
+            elif winner == "NoWinner":
+                await interaction.response.edit_message(content="It's a tie!", view=None)
             else:
-                await interaction.channel.send("Next player's turn.")
+                next_player = player2 if current_player == player1 else player1  # Switch to the next player
+                await interaction.response.edit_message(content=f"It's {next_player.mention}'s turn.", view=newView)
+                self.current_player = next_player  # Update the current player
 
     async def update_game_field(self, button, player):
         gameField = self.gameField
         index = int(button.custom_id) - 1
         if gameField[index] == 0:
             gameField[index] = player
+            button.disabled = True
             button.label = "X" if player == 1 else "O"
-            button.style = discord.ButtonStyle.green
+            button.style = discord.ButtonStyle.green if player == 1 else discord.ButtonStyle.blurple
         return self
-
-            # await button.edit(label="X" if player == 1 else "O", style=discord.ButtonStyle.green)
 
     async def check_winner(self):
         player1 = self.player1
         player2 = self.player2
         gameField = self.gameField
         # Check horizontal rows
-        for i in range(0, 9, 3):
+        for i in range(0, 9, 3): # 0, 3, 6
             if gameField[i] == gameField[i + 1] == gameField[i + 2] != 0:
                 return player1 if gameField[i] == 1 else player2
 
@@ -117,61 +119,15 @@ class TicTacToe(discord.ui.View):
             return player1 if gameField[0] == 1 else player2
         if gameField[2] == gameField[4] == gameField[6] != 0:
             return player1 if gameField[2] == 1 else player2
-
-        return None
-
-    async def buttonUpdate(self, id, label, style, row):
-        @discord.ui.button(custom_id=id, label=label, style=style, row=row)
-        async def callback(button, interaction):
-            print("Button clicked")
-
-        for i in range(1, 10):
-            i = discord.ui.Button(style=discord.ButtonStyle.secondary, label=str(i), custom_id=str(i))
-            self.add_item(i)
-            i.callback = lambda i: self.button_callback(i)
-
-    async def update_game_field(self, button, player):
-        gameField = self.gameField
-        index = int(button.custom_id) - 1
-        if gameField[index] == 0:
-            gameField[index] = player
-            button.label = "X" if player == 1 else "O"
-            button.style = discord.ButtonStyle.green
-        return self
-            # button(label="X" if player == 1 else "O", style=discord.ButtonStyle.green)
-            # await self.update()
-            # return await interaction.response.edit_message(view=self)
-            # await button.edit(label="X" if player == 1 else "O", style=discord.ButtonStyle.green)
-
-    async def check_winner(self):
-        player1 = self.player1
-        player2 = self.player2
-        gameField = self.gameField
-        # Check horizontal rows
-        for i in range(0, 9, 3):
-            if gameField[i] == gameField[i + 1] == gameField[i + 2] != 0:
-                return player1 if gameField[i] == 1 else player2
-
-        # Check vertical rows
-        for i in range(3):
-            if gameField[i] == gameField[i + 3] == gameField[i + 6] != 0:
-                return player1 if gameField[i] == 1 else player2
-
-        # Check diagonal rows
-        if gameField[0] == gameField[4] == gameField[8] != 0:
-            return player1 if gameField[0] == 1 else player2
-        if gameField[2] == gameField[4] == gameField[6] != 0:
-            return player1 if gameField[2] == 1 else player2
-
-        return None
-
-async def buttonUpdate(self, id, label, style, row):
-    @discord.ui.button(custom_id=id, label=label, style=style, row=row)
-    async def callback(button, interaction):
-        print("Button clicked")
-
-    return callback if self.player2 is not None else None
-
+        
+        full = True
+        for i in range(9):
+            if gameField[i] == 0:
+                full = False
+        if full:
+            return "NoWinner"
+        else:
+            return None
 
 async def startEmbed(interaction: discord.Interaction):
     player1 = interaction.user
@@ -193,9 +149,8 @@ async def ttt(interaction: discord.Interaction):
     
     player2 = start_button_view.player2  # Get the player2 property
     gameField = [0, 0, 0, 0, 0, 0, 0, 0, 0]  # Initialize the game field
-    # await interaction.channel.send("TTT-Game started", view=TicTacToe(player1, player2, gameField))
     print(f"Player 1 - ttt: {player1}")
-    print(f"Player 2 - ttt: {player2}") #muss noch None sein
+    # print(f"Player 2 - ttt: {player2}") #muss noch None sein
 
 # running bot with token
 bot.run(token[0])
