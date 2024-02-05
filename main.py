6a78df0e0155f6fc03999f4b16635d749adadc9b
NicoAtSchwarz <nic.oblaender.23@lehre.mosbach.dhbw.de>
2024-02-05 11:35:32 +0100

    integrated TicTacToe into main.py file

--- main.py
+++ main.py
@@ -13,15 +13,19 @@ bot = commands.Bot(command_prefix="!",intents = discord.Intents.all())
 # ready-message
 @bot.event
 async def on_ready():
-    print("I am ready for work! ~Pyhlia")
+    print(f"I am ready for work! ~{bot.user}")
+    status = discord.CustomActivity("Use /help for help")
+    await bot.change_presence(status=discord.Status.online, activity=status)
+    print(f"Status set to: {status}")
     try:
         synced = await bot.tree.sync()
         print(f"Currently listening to {len(synced)} command(s).")
     except Exception as e:
         print(e)
-    #user = await bot.fetch_user("721404393664020560")
-    #await user.send("Hello there, I am online now!")
 
+@bot.tree.command(name="tictactoe", description="TicTacToe")
+async def tttGame(interaction: discord.Interaction):
+    await tic_tac_toe.ttt(interaction)
 
 # running bot with token
 bot.run(token[0])
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,26 +3,8 @@ import datetime
 import discord
 from discord.ext import commands
 
-# loading token
-with open('token.txt') as file:
-    token = file.readlines()
-
 # bot declaration
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-# ready-message
-@bot.event
-async def on_ready():
-    print(f'{bot.user} is now online! TicTacToe')
-    status = discord.CustomActivity("Ich funktioniere!")
-    await bot.change_presence(status=discord.Status.online, activity=status)
-    print(f"Status set to: {status}")
-
-    try:
-        synced = await bot.tree.sync()
-        print(f"Currently listening to {len(synced)} command(s).")
-    except Exception as e:
-        print(e)
+bot = commands.Bot(command_prefix="!",intents = discord.Intents.all())
 
 mainColour = 0xa2c188
 embedGamesColour = 0xd9a4fc
@@ -174,13 +156,9 @@ async def startEmbed(player1: discord.user):
     )
     return embedStart
 
-@bot.tree.command(name="tictactoe", description="TicTacToe")
 async def ttt(interaction: discord.Interaction):
     # Get the user who triggered the slash command
     player1 = interaction.user
     startEmbedMessage = await startEmbed(player1)
     start_button_view = start_button(player1.id)  # Create the view
     await interaction.response.send_message(embed=startEmbedMessage, view=start_button_view)
-
-# running bot with token
-bot.run(token[0])
